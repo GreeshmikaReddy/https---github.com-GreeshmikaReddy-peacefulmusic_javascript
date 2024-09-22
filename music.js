ac5fe853ef46399a7ea355de575b4f62bcedd732
Greeshmika <boddugreeshmikareddy@gmail.com>
2024-09-22 16:15:16 +0530

    first commit

---
+++ music.js
@@ -0,0 +1,26 @@
+let trackList = [
+    {
+      name: "What Jhumka",
+      path: "./musics/What Jhumka - Rocky Aur Rani Kii Prem Kahaani 320 Kbps.mp3",
+      img: "./images/img1.jfif",
+      singer:  "Jonitha",
+    },
+    {
+      name: "Dear Comrade",
+      path: "./musics/Yetu Pone - SenSongsMp3.Co.mp3",
+      img: "./images/img2.jfif",
+      singer: "Arjith Singh",
+    },
+    {
+      name: "Oru Maalai",
+      path: "./musics/Oru-Maalai.mp3",
+      img: "./images/img3.jfif",
+      singer:"Karthik",
+    },
+    {
+      name: "Mr Perfect",
+      path: "./musics/[iSongs.info] 06 - Akasham Baddalaina.mp3",
+      img: "./images/img4.jfif",
+      singer: "Mega & Sagar",
+    },
+  ];
